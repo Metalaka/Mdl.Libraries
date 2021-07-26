be17c44bfeeb86c28ce2328665085db48313f356
Metalaka <matthieu.de.laubrie@gmail.com>
2021-07-25 21:19:47 -0400

    Import xUnit utilities

---
+++ src/Mdl.Tests.Xunit/ClassDataUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Mdl.Collections.Enumerators;
+
+namespace Mdl.Tests.Xunit
+{
+    /// <summary>
+    /// Utility class of <see cref="Xunit.ClassData"/>
+    /// </summary>
+    public static class ClassDataUtil
+    {
+        /// <summary>
+        /// Build an enumerator from several enumerable
+        /// </summary>
+        public static IEnumerator<object[]> BuildEnumerator(params IEnumerable[] enumerable)
+        {
+            var multipleMax = new MultipleMax(enumerable);
+            
+            foreach (var data in multipleMax)
+            {
+                yield return data.Cast<object[]>().Select(e => e[0]).ToArray();
+            }
+        }
+    }
+}
---
+++ tests/Mdl.Tests.Xunit.Tests/ClassDataUtilTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Mdl.Tests.Xunit.Tests
+{
+    public class ClassDataUtilTests
+    {
+        [Fact]
+        public void BuildEnumerator_ShouldReturnValuesForEachParametersAndRows()
+        {
+            var list = new List<object[]>
+            {
+                new object[] {1},
+                new object[] {2},
+            };
+            var list2 = new List<object[]>
+            {
+                new object[] {123},
+            };
+            var expected = new object[][]
+            {
+                new object[] {1, 123,},
+                new object[] {2, 123,},
+            };
+            var result = new List<object[]>();
+            using IEnumerator<object[]> sut = ClassDataUtil.BuildEnumerator(list, list2);
+
+            while (sut.MoveNext())
+            {
+                result.Add(sut.Current);
+            }
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
